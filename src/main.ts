bf6b3d93030079b04524c55754b9359b203c3f9d
Lyssa Li <lyssa.li137@gmail.com>
2024-10-21 22:22:29 -0700

    step 1 complete
    added application title, canvas, and canvas style

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,12 @@ const APP_NAME = "boopadoop";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+// app.innerHTML = APP_NAME;
+
+
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+app.append(title);
+
+const canvas = document.createElement("canvas");
+app.append(canvas);
