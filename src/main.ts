0a7cd1e4cdf6f92d23620502e0c356beb31f8623
Lyssa Li <lyssa.li137@gmail.com>
2024-10-24 12:40:20 -0700

    cleanup: reorganization of canvasCtx and in general

--- src/main.ts
+++ src/main.ts
@@ -5,62 +5,12 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 
-const drawingEvent: Event = new Event("drawing-changed");
-interface Point {
-  x: number;
-  y: number;
+// utility //////////////////////////////////////////////////////////////////////
+// @ts-ignore: all purpose function
+// deno-lint-ignore no-explicit-any
+function clearList(list: any[]) {
+  list.length = 0;
 }
-type Line = Point[];
-interface CanvasCtx {
-  lines: Line[];
-  undoBuffer: Line[];
-  display: (ctx: CanvasRenderingContext2D) => void;
-}
-const canvasContent: CanvasCtx = {
-  lines: [],
-  undoBuffer: [],
-  display: function (ctx: CanvasRenderingContext2D): void {
-    this.lines.forEach((line) => {
-      for (let i = 0; i < line.length - 1; ++i) {
-        drawLine(ctx, line[i].x, line[i].y, line[i + 1].x, line[i + 1].y);
-      }
-    });
-  },
-};
-
-const title = document.createElement("h1");
-title.textContent = APP_NAME;
-app.append(title);
-
-const canvas = document.createElement("canvas");
-canvas.width = 256;
-canvas.height = 256;
-app.append(canvas);
-
-const buttonContainer = document.createElement("div");
-app.append(buttonContainer);
-
-let isDrawing: boolean = false;
-const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
-canvas.addEventListener("mousedown", (e) => {
-  newLine(canvasContent, { x: e.offsetX, y: e.offsetY });
-  isDrawing = true;
-});
-canvas.addEventListener("mousemove", (e) => {
-  if (isDrawing) {
-    extendNextLine(canvasContent, { x: e.offsetX, y: e.offsetY });
-  }
-});
-document.addEventListener("mouseup", (e) => {
-  if (isDrawing) {
-    extendNextLine(canvasContent, { x: e.offsetX, y: e.offsetY });
-    isDrawing = false;
-  }
-});
-document.addEventListener("drawing-changed", () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  canvasContent.display(ctx);
-});
 
 function drawLine(
   ctx: CanvasRenderingContext2D,
@@ -78,59 +28,115 @@ function drawLine(
   ctx.closePath();
 }
 
+const drawingEvent: Event = new Event("drawing-changed");
+interface Point {
+  x: number;
+  y: number;
+}
+type Line = Point[];
+interface CanvasCtx {
+  lines: Line[];
+  undoBuffer: Line[];
+  display: (ctx: CanvasRenderingContext2D) => void;
+  newLine: (point: Point) => void;
+  extendNextLine: (point: Point) => void;
+  undo: () => void;
+  redo: () => void;
+  clear: () => void;
+}
+const canvasContent: CanvasCtx = {
+  lines: [],
+  undoBuffer: [],
+  display: function (ctx: CanvasRenderingContext2D): void {
+    this.lines.forEach((line) => {
+      for (let i = 0; i < line.length - 1; ++i) {
+        drawLine(ctx, line[i].x, line[i].y, line[i + 1].x, line[i + 1].y);
+      }
+    });
+  },
+  newLine: function (point: Point): void {
+    this.lines.push([point]);
+    document.dispatchEvent(drawingEvent);
+    clearList(this.undoBuffer);
+  },
+  extendNextLine: function (point: Point): void {
+    if (this.lines.length === 0) return;
+    this.lines[this.lines.length - 1].push(point);
+    document.dispatchEvent(drawingEvent);
+  },
+  undo: function () {
+    if (this.lines.length === 0) return;
+    this.undoBuffer.unshift(this.lines.pop()!);
+    document.dispatchEvent(drawingEvent);
+  },
+  redo: function () {
+    if (this.undoBuffer.length === 0) return;
+    this.lines.push(this.undoBuffer.shift()!);
+    document.dispatchEvent(drawingEvent);
+  },
+  clear: function () {
+    clearList(this.lines);
+    clearList(this.undoBuffer);
+    document.dispatchEvent(drawingEvent);
+  },
+};
+
+// application interface //////////////////////////////////////////////////////
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+app.append(title);
+
+const canvas = document.createElement("canvas");
+const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
+
+const buttonContainer = document.createElement("div");
+app.append(buttonContainer);
+
 const clearButton = document.createElement("button");
 clearButton.textContent = "clear";
 clearButton.addEventListener("click", () => {
-  clear(canvasContent);
+  canvasContent.clear();
 });
 buttonContainer.append(clearButton);
 
 const undoButton = document.createElement("button");
 undoButton.textContent = "undo";
 undoButton.addEventListener("click", () => {
-  undo(canvasContent);
+  canvasContent.undo();
 });
 buttonContainer.append(undoButton);
 
 const redoButton = document.createElement("button");
 redoButton.textContent = "redo";
 redoButton.addEventListener("click", () => {
-  redo(canvasContent);
+  canvasContent.redo();
 });
 buttonContainer.append(redoButton);
 
-// @ts-ignore: all purpose function
-// deno-lint-ignore no-explicit-any
-function clearList(list: any[]) {
-  list.length = 0;
-}
+const markerContainer = document.createElement("div");
+app.append(markerContainer);
 
-function newLine(canvas: CanvasCtx, point: Point): void {
-  canvas.lines.push([point]);
-  document.dispatchEvent(drawingEvent);
-  clearList(canvas.undoBuffer);
-}
-
-function extendNextLine(canvas: CanvasCtx, point: Point): void {
-  if (canvas.lines.length === 0) return;
-  canvas.lines[canvas.lines.length - 1].push(point);
-  document.dispatchEvent(drawingEvent);
-}
-
-function undo(canvas: CanvasCtx) {
-  if (canvas.lines.length === 0) return;
-  canvas.undoBuffer.unshift(canvas.lines.pop()!);
-  document.dispatchEvent(drawingEvent);
-}
-
-function redo(canvas: CanvasCtx) {
-  if (canvas.undoBuffer.length === 0) return;
-  canvas.lines.push(canvas.undoBuffer.shift()!);
-  document.dispatchEvent(drawingEvent);
-}
-
-function clear(canvas: CanvasCtx) {
-  clearList(canvas.lines);
-  clearList(canvas.undoBuffer);
-  document.dispatchEvent(drawingEvent);
-}
+// event listeners /////////////////////////////////////////////////////////////
+let isDrawing: boolean = false;
+canvas.addEventListener("mousedown", (e) => {
+  canvasContent.newLine({ x: e.offsetX, y: e.offsetY });
+  isDrawing = true;
+});
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    canvasContent.extendNextLine({ x: e.offsetX, y: e.offsetY });
+  }
+});
+document.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    canvasContent.extendNextLine({ x: e.offsetX, y: e.offsetY });
+    isDrawing = false;
+  }
+});
+document.addEventListener("drawing-changed", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  canvasContent.display(ctx);
+});
